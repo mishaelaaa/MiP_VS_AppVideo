1c10fe0002a2f1793d99f72e435f88cfba71df63
Mihaela Petkova <mishippp@gmail.com>
2018-03-25 19:34:34 +0300

    The video with play,pause,stop
    the basis where I will add applications of the video.

--- MiP_Computr_vision_V0.0/Form1.Designer.cs
+++ MiP_Computr_vision_V0.0/Form1.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Play = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Coose_File = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Mute = new System.Windows.Forms.Button();
+            this.Graphics = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(21, 104);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(602, 394);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(148, 12);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(85, 45);
+            this.Play.TabIndex = 1;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(239, 12);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(78, 45);
+            this.Pause.TabIndex = 2;
+            this.Pause.Text = "Pause";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(323, 12);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(80, 45);
+            this.Stop.TabIndex = 3;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Coose_File
+            // 
+            this.Coose_File.Location = new System.Drawing.Point(498, 62);
+            this.Coose_File.Name = "Coose_File";
+            this.Coose_File.Size = new System.Drawing.Size(125, 23);
+            this.Coose_File.TabIndex = 4;
+            this.Coose_File.Text = "Choose File";
+            this.Coose_File.UseVisualStyleBackColor = true;
+            this.Coose_File.Click += new System.EventHandler(this.Coose_File_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(471, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Mute
+            // 
+            this.Mute.Location = new System.Drawing.Point(407, 12);
+            this.Mute.Name = "Mute";
+            this.Mute.Size = new System.Drawing.Size(85, 45);
+            this.Mute.TabIndex = 6;
+            this.Mute.Text = "Mute";
+            this.Mute.UseVisualStyleBackColor = true;
+            this.Mute.Click += new System.EventHandler(this.Mute_Click);
+            // 
+            // Graphics
+            // 
+            this.Graphics.Location = new System.Drawing.Point(498, 15);
+            this.Graphics.Name = "Graphics";
+            this.Graphics.Size = new System.Drawing.Size(82, 42);
+            this.Graphics.TabIndex = 7;
+            this.Graphics.Text = "Graphics";
+            this.Graphics.UseVisualStyleBackColor = true;
+            this.Graphics.Click += new System.EventHandler(this.Graphics_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 520);
+            this.Controls.Add(this.Graphics);
+            this.Controls.Add(this.Mute);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Coose_File);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Coose_File;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Mute;
+        private System.Windows.Forms.Button Graphics;
     }
 }
 
--- MiP_Computr_vision_V0.0/Form1.cs
+++ MiP_Computr_vision_V0.0/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
+using System.Drawing.Drawing2D;
 
 namespace MiP_Computr_vision_V0._0
 {
@@ -15,6 +17,66 @@ namespace MiP_Computr_vision_V0._0
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public System.Boolean mute { get; set; }
+    #pragma warning disable CS1633 // Unrecognized #pragma directive
+    #pragma the choose file of my doc
+        private void Coose_File_Click(object sender, EventArgs e)
+    #pragma warning restore CS1633 // Unrecognized #pragma directive
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+    #pragma warning disable CS1633 // Unrecognized #pragma directive
+        private void Play_Click(object sender, EventArgs e)
+    #pragma warning restore CS1633 // Unrecognized #pragma directive
+        {
+            axWindowsMediaPlayer1.URL = textBox1.Text;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+    #pragma warning disable CS1633 // Unrecognized #pragma directive
+        private void Pause_Click(object sender, EventArgs e)
+    #pragma warning restore CS1633 // Unrecognized #pragma directive
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+    #pragma warning disable CS1633 // Unrecognized #pragma directive
+        private void Stop_Click(object sender, EventArgs e)
+    #pragma warning restore CS1633 // Unrecognized #pragma directive
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void Mute_Click(object sender, EventArgs e)
+        {
+    #pragma warning disable CS8321 // Local function is declared but never used
+            void Mute_CheckStateChanged(EventArgs @event)
+    #pragma warning restore CS8321 // Local function is declared but never used
+            {
+                System.Windows.Forms.CheckBox Mute = (System.Windows.Forms.CheckBox)sender;
+
+                // Change the check box text depending on the checked state.
+                Mute.Text = Mute.Checked ? "Un-mute Audio" : Mute.Text = "Mute Audio";
+
+                // Use the checked state to set the mute property. 
+                axWindowsMediaPlayer1.settings.mute = Mute.Checked;
+                axWindowsMediaPlayer1.Ctlcontrols.GetType();
+            }
+        }
+
+        private void Graphics_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
